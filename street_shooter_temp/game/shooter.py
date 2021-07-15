d74c98c71be5d4b55e49b2d9b503b45c00847934
benjaminjalebeau <72671113+benjaminjalebeau@users.noreply.github.com>
2021-07-15 10:36:01 -0600

    Update shooter.py

--- street_shooter_temp/game/shooter.py
+++ street_shooter_temp/game/shooter.py
@@ -16,6 +16,7 @@ class Shooter(Flying_Object):
         self.velocity.dy = 0
         self.fire_rate = BULLET_FIRE_RATE
         self.lives = START_LIVES
+        self.damage = 1
 
     def move_right(self):
         if self.center.x < SCREEN_WIDTH - SHOOTER_SIZE / 2:
@@ -49,10 +50,14 @@ class Shooter(Flying_Object):
         y = self.center.y
         angle = self.angle + 90
 
+        arcade.draw_text(f"Power: {self.damage}", SCREEN_WIDTH*.8 , SCREEN_HEIGHT*.95, arcade.color.BLUE, 20)
+
         arcade.draw_text(f"Lives: {self.lives}", 15, SCREEN_HEIGHT*.9, arcade.color.RED, 20)
         if self.alive:
             arcade.draw_texture_rectangle(x, y, width, height, texture, angle, alpha)
 
         else:
             arcade.draw_texture_rectangle(x + 10, y+5, 50, 50, texture2, angle, alpha)
+
+
 
