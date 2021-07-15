1bb605097d543ae87adcc82936e408457361df0b
markvagil <83776097+markvagil@users.noreply.github.com>
2021-07-15 11:31:01 -0500

    Delete street_shooter_game directory

--- street_shooter_game/__main__.py
+++
@@ -1,9 +0,0 @@
-from game.constants import *
-from game.director import InstructionView
-import arcade
-
-# Creates the game and starts it
-window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, "Street Shooter")
-start_view = InstructionView()
-window.show_view(start_view)
-arcade.run()
