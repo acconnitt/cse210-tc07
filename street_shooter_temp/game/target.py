d92fb9458fcad27d955e0b5491bafec5ce7303f9
markvagil <83776097+markvagil@users.noreply.github.com>
2021-07-15 11:31:57 -0500

    Add files via upload

---
+++ street_shooter_temp/game/target.py
@@ -0,0 +1,40 @@
+import arcade
+import random
+from game.constants import *
+from game.flying_object import Flying_Object
+
+class Target(Flying_Object):
+
+    def __init__(self):
+        super().__init__()
+        self.radius = TARGET_RADIUS
+        self.velocity.dy = TARGET_SPEED
+        self.velocity.dx = 0
+        self.lives = 1
+
+    def draw(self):
+        """ """
+        
+        img = "assets/crate.png"
+        texture = arcade.load_texture(img)
+        
+        width = texture.width // 6
+        height = texture.height // 6
+        alpha = 255
+
+        x = self.center.x
+        y = self.center.y
+        angle = self.angle + 90
+
+        arcade.draw_texture_rectangle(x, y, width, height, texture, angle, alpha)
+        arcade.draw_text(str(self.lives),
+                         self.center.x, self.center.y, arcade.color.WHITE, 20, width=100, align="center", anchor_x="center", anchor_y="center")
+
+    def collide (self, score):
+        self.lives -= 1
+        if self.lives <= 0:
+            score.update_score()
+            self.alive = False
+
+    def generate_lives(self, score):
+        self.lives = random.randint(1, score.score // 2 + 1)
