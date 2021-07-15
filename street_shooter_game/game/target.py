62130936021f20dc0acd008363d33b5a2c9f4ea5
benjaminjalebeau <72671113+benjaminjalebeau@users.noreply.github.com>
2021-07-15 10:20:36 -0600

    Update target.py

--- street_shooter_game/game/target.py
+++ street_shooter_game/game/target.py
@@ -1,5 +1,7 @@
 import arcade
 import random
+
+from arcade.color import RED
 from game.constants import *
 from game.flying_object import Flying_Object
 
@@ -30,11 +32,101 @@ class Target(Flying_Object):
         arcade.draw_text(str(self.lives),
                          self.center.x, self.center.y, arcade.color.WHITE, 20, width=100, align="center", anchor_x="center", anchor_y="center")
 
-    def collide (self, score):
-        self.lives -= 1
+    def collide (self, score, shooter):
+        self.lives -= shooter.damage
         if self.lives <= 0:
             score.update_score()
             self.alive = False
 
-    def generate_lives(self, score):
-        self.lives = random.randint(1, score.score // 2 + 1)
+    def generate_lives(self, score, shooter):
+        self.lives = random.randint(1, (score.score * shooter.damage + 1))
+
+class RedTarget(Target):
+
+    def __init__(self):
+        super().__init__()
+        
+
+    def draw(self):
+
+        img = "assets/crate-red.png"
+        texture = arcade.load_texture(img)
+        
+        width = texture.width // 6
+        height = texture.height // 6
+        alpha = 255
+
+        x = self.center.x
+        y = self.center.y
+        angle = self.angle + 90
+
+        arcade.draw_texture_rectangle(x, y, width, height, texture, angle, alpha)
+        arcade.draw_text(str(self.lives),
+                         self.center.x, self.center.y, arcade.color.WHITE, 20, width=100, align="center", anchor_x="center", anchor_y="center")
+
+    def collide (self, score, shooter):
+        self.lives -= shooter.damage
+        if self.lives <= 0:
+            score.update_score()
+            if shooter.lives < MAX_LIVES:
+                shooter.lives += 1
+            self.alive = False
+
+class BlueTarget(Target):
+
+    def __init__(self):
+        super().__init__()
+
+    def draw(self):
+        img = "assets/crate-blue.png"
+        texture = arcade.load_texture(img)
+        
+        width = texture.width // 6
+        height = texture.height // 6
+        alpha = 255
+
+        x = self.center.x
+        y = self.center.y
+        angle = self.angle + 90
+
+        arcade.draw_texture_rectangle(x, y, width, height, texture, angle, alpha)
+        arcade.draw_text(str(self.lives),
+                         self.center.x, self.center.y, arcade.color.WHITE, 20, width=100, align="center", anchor_x="center", anchor_y="center")
+
+    def collide (self, score, shooter):
+        self.lives -= shooter.damage
+        if self.lives <= 0:
+            score.update_score()
+            shooter.damage += 1
+            self.alive = False
+            
+class GreenTarget(Target):
+
+    def __init__(self):
+        super().__init__()
+
+    def draw(self):
+        img = "assets/crate-green.png"
+        texture = arcade.load_texture(img)
+        
+        width = texture.width // 6
+        height = texture.height // 6
+        alpha = 255
+
+        x = self.center.x
+        y = self.center.y
+        angle = self.angle + 90
+
+        arcade.draw_texture_rectangle(x, y, width, height, texture, angle, alpha)
+        arcade.draw_text(str(self.lives),
+                         self.center.x, self.center.y, arcade.color.WHITE, 20, width=100, align="center", anchor_x="center", anchor_y="center")
+
+    def collide (self, score, shooter):
+        self.lives -= shooter.damage
+        if self.lives <= 0:
+            score.update_score()
+            if shooter.fire_rate > MAX_FIRE_RATE:
+                shooter.fire_rate = shooter.fire_rate / 1.1
+            self.alive = False
+            
+
